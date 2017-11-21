4a37424dff4d1752c6388dcaeda4742a306bb662
TickTack_z <sxzhou@live.com>
2017-11-20 21:01:39 -0500

    First commit

---
+++ background.js
@@ -0,0 +1,19 @@
+// background.js
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs;
+    alert(activeTab[0].id);
+    chrome.tabs.update(activeTab, {selected: true});
+  });
+});
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "open_new_tab" ) {
+      chrome.tabs.create({"url": request.url});
+    }
+  }
+);
