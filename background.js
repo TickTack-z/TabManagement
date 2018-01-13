1860fbd4bec8a1fdbf589041f588a38817e58f52
Aaron Zhou <sxzhou@live.com>
2018-01-13 10:15:29 +0800

    init

--- background.js
+++ background.js
@@ -1,19 +1,32 @@
-// background.js
-
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs;
-    alert(activeTab[0].id);
-    chrome.tabs.update(activeTab, {selected: true});
-  });
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete') {
+        chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+                var myElem = document.getElementById('mp3');
+                if (myElem.paused) {
+                } else{
+                    getAudioUrl(tabs[0].url, new_music);
+                }
+            }
+        )
+    }
 });
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-      chrome.tabs.create({"url": request.url});
-    }
-  }
-);
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(function() {
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        // With a new rule ...
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                // That fires when a page's URL contains a 'g' ...
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: { urlContains: 'g' },
+                    })
+                ],
+                // And shows the extension's page action.
+                actions: [ new chrome.declarativeContent.ShowPageAction() ]
+            }
+        ]);
+    });
+});
